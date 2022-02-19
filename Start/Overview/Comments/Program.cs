476c88f8c4b9c67048f41675d0ed5e2bc9d85d80
haerang choi <gofkdvjvl@naver.com>
2022-02-20 00:43:41 +0900

    feat: basic folders

---
+++ Start/Overview/Comments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            /*
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            */
+
+        }
+    }
+}
---
+++ Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("what is your name?");
+string str=Console.ReadLine();
+Console.WriteLine("why, hello there "+str);
+
+Console.ReadLine();
